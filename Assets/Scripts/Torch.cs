4b4fcae13e8dbb3fe2d18196e06184dd96e17443
John Miller <mil17003@byui.edu>
2020-10-01 11:43:37 -0600

    Torches are fully functional
    Torches now signal a counter that will be used to unlock the door eventually.

--- Assets/Scripts/Torch.cs
+++ Assets/Scripts/Torch.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Torch : MonoBehaviour
 {
     public GameObject litTorch;
+    private bool lit = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Torch")
+        if (collision.tag == "Torch" && lit == false)
+        {
             litTorch.GetComponent<SpriteRenderer>().enabled = true;
+            TorchCounter.torchCount += 1;
+            lit = true;
+        }
     }
 }
