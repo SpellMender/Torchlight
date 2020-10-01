1ba4bc2b0657e4351625eaa78ba7003ac1fc6481
SpellMender <18728350+SpellMender@users.noreply.github.com>
2020-10-01 12:16:08 -0600

    Tutorial Level Completed!
    Just need door to open

---
+++ Assets/BlockerDrop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockerDrop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sortingOrder = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<Rigidbody2D>().velocity.y < -0.1f)
+        {
+            GetComponent<SpriteRenderer>().sortingOrder = 1;
+        }
+    }
+}
